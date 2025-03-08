65f62141c611ae577e1e3b8762e744cc0f4aaa15
Miraç <miracsucu4417@gmail.com>
2025-03-08 22:14:50 +0300

    v2

--- V2/bsq.h
+++ V2/bsq.h
@@ -1,17 +1,20 @@
 #ifndef BSQ_H
 #define BSQ_H
+
 #include <unistd.h>
 
-struct bsq_map_d
+typedef struct bsq_map_d
 {
     
     int line_len;
+	int col_len;
     char *file_name;
     char space;
     char obstacle;
     char full;
     char **map_data;
-};
+}	bsq_map_d;
 
+void	ft_puterr(char *str);
 
 #endif
